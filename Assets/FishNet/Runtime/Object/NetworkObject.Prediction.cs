4709355945f2aedccea6dda5b0677fca86fc92be
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-04-10 15:26:24 +0200

    Bump Fishnet 4.1.2 -> 4.2.0

--- Assets/FishNet/Runtime/Object/NetworkObject.Prediction.cs
+++ Assets/FishNet/Runtime/Object/NetworkObject.Prediction.cs
@@ -1,17 +1,19 @@
 ﻿using FishNet.Component.Prediction;
+using FishNet.Component.Transforming;
 using FishNet.Managing;
 using FishNet.Managing.Timing;
 using FishNet.Object.Prediction;
+using GameKit.Dependencies.Utilities;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace FishNet.Object
 {
-#if PREDICTION_V2
+#if !PREDICTION_1
     public partial class NetworkObject : MonoBehaviour
     {
         #region Types.
-#if PREDICTION_V2
+#if !PREDICTION_1
         /// <summary>
         /// Type of prediction movement being used.
         /// </summary>
@@ -26,6 +28,12 @@ namespace FishNet.Object
         #endregion
 
         #region Public.
+#if !PREDICTION_1
+        /// <summary>
+        /// True if a reconcile is occuring on any NetworkBehaviour that is on or nested of this NetworkObject. Runtime NetworkBehaviours are not included, such as if you child a NetworkObject to another at runtime.
+        /// </summary>
+        public bool IsObjectReconciling { get; internal set; }
+#endif
         /// <summary>
         /// Last tick this object replicated.
         /// </summary>
@@ -38,13 +46,14 @@ namespace FishNet.Object
 
         #region Internal.
         /// <summary>
-        /// Pauses rigidbodies for prediction.
+        /// Pauses and unpauses rigidbodies when they do not have data to reconcile to.
         /// </summary>
-        public RigidbodyPauser RigidbodyPauser { get; private set; }
+        public RigidbodyPauser RigidbodyPauser => _rigidbodyPauser;
+        private RigidbodyPauser _rigidbodyPauser;
         #endregion
 
         #region Serialized.
-#if PREDICTION_V2
+#if !PREDICTION_1
         /// <summary>
         /// True if this object uses prediciton methods.
         /// </summary>
@@ -67,9 +76,16 @@ namespace FishNet.Object
         /// <summary>
         /// True to forward replicate and reconcile states to all clients. This is ideal with games where you want all clients and server to run the same inputs. False to only use prediction on the owner, and synchronize to spectators using other means such as a NetworkTransform.
         /// </summary>
+        public bool EnableStateForwarding => (_enablePrediction && _enableStateForwarding);
         [Tooltip("True to forward replicate and reconcile states to all clients. This is ideal with games where you want all clients and server to run the same inputs. False to only use prediction on the owner, and synchronize to spectators using other means such as a NetworkTransform.")]
         [SerializeField]
         private bool _enableStateForwarding = true;
+        /// <summary>
+        /// NetworkTransform to configure for prediction. Specifying this is optional.
+        /// </summary>
+        [Tooltip("NetworkTransform to configure for prediction. Specifying this is optional.")]
+        [SerializeField]
+        private NetworkTransform _networkTransform;
         /// <summary>
         /// How many ticks to interpolate graphics on objects owned by the client. Typically low as 1 can be used to smooth over the frames between ticks.
         /// </summary>
@@ -97,19 +113,11 @@ namespace FishNet.Object
         /// <summary>
         /// Graphical smoother to use when using set for owner.
         /// </summary>
-        private PredictionTickSmoother _tickSmoother;
+        private LocalTransformTickSmoother _tickSmoother;
         /// <summary>
         /// NetworkBehaviours which use prediction.
         /// </summary>
         private List<NetworkBehaviour> _predictionBehaviours = new List<NetworkBehaviour>();
-        ///// <summary>
-        ///// Tick when CollionStayed last called. This only has value if using prediction.
-        ///// </summary>
-        //private uint _collisionStayedTick;
-        ///// <summary>
-        ///// Local client objects this object is currently colliding with.
-        ///// </summary>
-        //private HashSet<GameObject> _localClientCollidedObjects = new HashSet<GameObject>();
         #endregion
 
         private void Prediction_Update()
@@ -127,13 +135,15 @@ namespace FishNet.Object
         private void TimeManager_OnPostTick()
         {
             _tickSmoother?.OnPostTick();
-            //TrySetCollisionExited();
         }
 
         private void Prediction_Preinitialize(NetworkManager manager, bool asServer)
         {
             if (!_enablePrediction)
                 return;
+
+            if (!_enableStateForwarding && _networkTransform != null)
+                _networkTransform.ConfigureForPrediction(_predictionType);
 
             ReplicateTick.Initialize(manager.TimeManager);
             InitializeSmoothers();
@@ -143,7 +153,7 @@ namespace FishNet.Object
 
             if (_predictionBehaviours.Count > 0)
             {
-                manager.PredictionManager.OnPreReconcile += PredictionManager_OnPreReconcile;
+                manager.PredictionManager.OnReconcile += PredictionManager_OnReconcile;
                 manager.PredictionManager.OnReplicateReplay += PredictionManager_OnReplicateReplay;
                 manager.PredictionManager.OnPostReconcile += PredictionManager_OnPostReconcile;
                 manager.TimeManager.OnPreTick += TimeManager_OnPreTick;
@@ -162,7 +172,7 @@ namespace FishNet.Object
              * dropping their connection. */
             if (_predictionBehaviours.Count > 0 && NetworkManager != null)
             {
-                NetworkManager.PredictionManager.OnPreReconcile -= PredictionManager_OnPreReconcile;
+                NetworkManager.PredictionManager.OnReconcile -= PredictionManager_OnReconcile;
                 NetworkManager.PredictionManager.OnReplicateReplay -= PredictionManager_OnReplicateReplay;
                 NetworkManager.PredictionManager.OnPostReconcile -= PredictionManager_OnPostReconcile;
                 NetworkManager.TimeManager.OnPreTick -= TimeManager_OnPreTick;
@@ -180,20 +190,21 @@ namespace FishNet.Object
             bool usesRb2d = (_predictionType == PredictionType.Rigidbody2D);
             if (usesRb || usesRb2d)
             {
-                RigidbodyPauser = new RigidbodyPauser();
+                _rigidbodyPauser = ResettableObjectCaches<RigidbodyPauser>.Retrieve();
                 RigidbodyType rbType = (usesRb) ? RigidbodyType.Rigidbody : RigidbodyType.Rigidbody2D;
-                RigidbodyPauser.UpdateRigidbodies(transform, rbType, true);
+                _rigidbodyPauser.UpdateRigidbodies(transform, rbType, true);
             }
 
             if (_graphicalObject == null)
             {
-                Debug.Log($"GraphicalObject is null on {this.ToString()}. This may be intentional, and acceptable, if you are smoothing between ticks yourself. Otherwise consider assigning the GraphicalObject field.");
+                NetworkManagerExtensions.Log($"GraphicalObject is null on {gameObject.name}. This may be intentional, and acceptable, if you are smoothing between ticks yourself. Otherwise consider assigning the GraphicalObject field.");
             }
             else
             {
-                _tickSmoother = new PredictionTickSmoother();
+                if (_tickSmoother == null)
+                    _tickSmoother = ResettableObjectCaches<LocalTransformTickSmoother>.Retrieve();
                 float teleportT = (_enableTeleport) ? _teleportThreshold : MoveRatesCls.UNSET_VALUE;
-                _tickSmoother.InitializeOnce(_graphicalObject, teleportT, this, _ownerInterpolation);
+                _tickSmoother.InitializeOnce(_graphicalObject, teleportT, (float)TimeManager.TickDelta, _ownerInterpolation);
             }
         }
 
@@ -202,13 +213,25 @@ namespace FishNet.Object
         /// </summary>
         private void DeinitializeSmoothers()
         {
-            _tickSmoother?.Deinitialize();
+            if (_tickSmoother != null)
+            {
+                ResettableObjectCaches<LocalTransformTickSmoother>.StoreAndDefault(ref _tickSmoother);
+                ResettableObjectCaches<RigidbodyPauser>.StoreAndDefault(ref _rigidbodyPauser);
+            }
         }
 
-        private void PredictionManager_OnPreReconcile(uint clientReconcileTick, uint serverReconcileTick)
+        private void PredictionManager_OnReconcile(uint clientReconcileTick, uint serverReconcileTick)
         {
-            for (int i = 0; i < _predictionBehaviours.Count; i++)
-                _predictionBehaviours[i].Reconcile_Client_Start();
+            if (!IsObjectReconciling)
+            {
+                if (_rigidbodyPauser != null)
+                    _rigidbodyPauser.Pause();
+            }
+            else
+            {
+                for (int i = 0; i < _predictionBehaviours.Count; i++)
+                    _predictionBehaviours[i].Reconcile_Client_Start();
+            }
         }
 
         private void PredictionManager_OnPostReconcile(uint clientReconcileTick, uint serverReconcileTick)
@@ -220,7 +243,9 @@ namespace FishNet.Object
              * than per NB, where the pausing occurs, because once here
              * the entire object is out of the replay cycle so there's
              * no reason to try and unpause per NB. */
-            RigidbodyPauser?.Unpause();
+            if (_rigidbodyPauser != null)
+                _rigidbodyPauser.Unpause();
+            IsObjectReconciling = false;
         }
 
 
@@ -230,124 +255,6 @@ namespace FishNet.Object
             for (int i = 0; i < _predictionBehaviours.Count; i++)
                 _predictionBehaviours[i].Replicate_Replay_Start(replayTick + 1);
         }
-
-        ///// <summary>
-        ///// Returns if this object is colliding with any local client objects.
-        ///// </summary>
-        ///// <returns></returns>
-        //internal bool CollidingWithLocalClient()
-        //{
-        //    /* If it's been more than 1 tick since collision stayed
-        //     * then do not consider as collided. */
-        //    return (TimeManager.LocalTick - _collisionStayedTick) <= 1;
-        //}
-
-        ///// <summary>
-        ///// Called when colliding with another object.
-        ///// </summary>
-        //private void OnCollisionEnter(Collision collision)
-        //{
-        //    if (!IsClientInitialized)
-        //        return;
-        //    if (_predictionType != PredictionType.Rigidbody)
-        //        return;
-
-        //    GameObject go = collision.gameObject;
-        //    if (CollisionEnteredLocalClientObject(go))
-        //        CollisionEntered(go);
-        //}
-
-        ///// <summary>
-        ///// Called when collision has entered a local clients object.
-        ///// </summary>
-        //private void CollisionEntered(GameObject go)
-        //{
-        //    if (_graphicalObject == null)
-        //        return;
-
-        //    _collisionStayedTick = TimeManager.LocalTick;
-        //    _localClientCollidedObjects.Add(go);
-        //}
-
-        ///// <summary>
-        ///// Called when colliding with another object.
-        ///// </summary>
-        //private void OnCollisionEnter2D(Collision2D collision)
-        //{
-        //    if (_graphicalObject == null)
-        //        return;
-        //    if (!IsClientInitialized)
-        //        return;
-        //    if (_predictionType != PredictionType.Rigidbody2D)
-        //        return;
-
-        //    GameObject go = collision.gameObject;
-        //    if (CollisionEnteredLocalClientObject(go))
-        //        CollisionEntered(go);
-        //}
-
-
-        ///// <summary>
-        ///// Called when staying in collision with another object.
-        ///// </summary>
-        //private void OnCollisionStay(Collision collision)
-        //{
-        //    if (!IsClientInitialized)
-        //        return;
-        //    if (_predictionType != PredictionType.Rigidbody)
-        //        return;
-
-        //    if (_localClientCollidedObjects.Contains(collision.gameObject))
-        //        _collisionStayedTick = TimeManager.LocalTick;
-        //}
-        ///// <summary>
-        ///// Called when staying in collision with another object.
-        ///// </summary>
-        //private void OnCollisionStay2D(Collision2D collision)
-        //{
-        //    if (!IsClientInitialized)
-        //        return;
-        //    if (_predictionType != PredictionType.Rigidbody2D)
-        //        return;
-
-        //    if (_localClientCollidedObjects.Contains(collision.gameObject))
-        //        _collisionStayedTick = TimeManager.LocalTick;
-        //}
-
-        ///// <summary>
-        ///// Called when a collision occurs and the smoothing type must perform operations.
-        ///// </summary>
-        //private bool CollisionEnteredLocalClientObject(GameObject go)
-        //{
-        //    if (go.TryGetComponent<NetworkObject>(out NetworkObject nob))
-        //        return nob.Owner.IsLocalClient;
-
-        //    //Fall through.
-        //    return false;
-        //}
-
-
-        ///// <summary>
-        ///// Called when collision has exited a local clients object.
-        ///// </summary>
-        //private void TrySetCollisionExited()
-        //{
-        //    /* If this object is no longer
-        //     * colliding with local client objects
-        //     * then unset collision.
-        //     * This is done here instead of using
-        //     * OnCollisionExit because often collisionexit
-        //     * will be missed due to ignored ticks. 
-        //     * While not ignoring ticks is always an option
-        //     * its not ideal because ignoring ticks helps
-        //    * prevent over predicting. */
-        //    TimeManager tm = TimeManager;
-        //    if (tm == null || (_collisionStayedTick != 0 && (tm.LocalTick != _collisionStayedTick)))
-        //    {
-        //        _localClientCollidedObjects.Clear();
-        //        _collisionStayedTick = 0;
-        //    }
-        //}
 
         /// <summary>
         /// Registers a NetworkBehaviour that uses prediction with the NetworkObject.
--- Assets/FishNet/Runtime/Object/NetworkObject.QOL.cs
+++ Assets/FishNet/Runtime/Object/NetworkObject.QOL.cs
@@ -1,4 +1,5 @@
-﻿using FishNet.Component.ColliderRollback;
+﻿using FishNet.CodeAnalysis.Annotations;
+using FishNet.Component.ColliderRollback;
 using FishNet.Connection;
 using FishNet.Managing;
 using FishNet.Managing.Client;
@@ -87,10 +88,20 @@ namespace FishNet.Object
         /// True if client nor server are started.
         /// </summary>
         public bool IsOffline => (!IsClientStarted && !IsServerStarted);
+#if !PREDICTION_1
+        /// <summary>
+        /// True if a reconcile is occuring on the PredictionManager. Note the difference between this and IsBehaviourReconciling.
+        /// </summary>
+        public bool IsManagerReconciling => PredictionManager.IsReconciling;
+#endif
         /// <summary>
         /// True if the local client is the owner of this object.
         /// This will only return true if IsClientInitialized is also true. You may check ownership status regardless of client initialized state by using Owner.IsLocalClient.
         /// </summary>
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "OnStartServer", "")]
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "OnStartNetwork", " Use base.Owner.IsLocalClient instead.")]
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "Awake", "")]
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "Start", "")]
         public bool IsOwner
         {
             get
@@ -124,7 +135,14 @@ namespace FishNet.Object
                 return Owner.IsLocalClient;
             }
         }
-
+        /// <summary>
+        /// True if IsOwner, or if IsServerInitialized with no Owner.
+        /// </summary>
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "OnStartServer", "")]
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "OnStartNetwork", " Use (base.Owner.IsLocalClient || (base.IsServerInitialized && !Owner.Isvalid) instead.")]
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "Awake", "")]
+        [PreventUsageInside("global::FishNet.Object.NetworkBehaviour", "Start", "")]
+        public bool IsOwnerOrServer => (IsOwner || (IsServerInitialized && !Owner.IsValid));
         /// <summary>
         /// 
         /// </summary>
