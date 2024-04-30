a5c875e695bf895b0ace8c90448e9f5aa126d896
Seedate <138722515+Seedate@users.noreply.github.com>
2024-04-30 18:44:59 +0200

    Update WwPlayerPanning
    still not working but we are in the good way i think

--- Assets/_Project/Scripts/Audio/WwPlayerPanning.cs
+++ Assets/_Project/Scripts/Audio/WwPlayerPanning.cs
@@ -12,18 +12,24 @@ public class WwPlayerPanning : MonoBehaviour
     public GameObject listenerPlayer2;
 
 
-    [SerializeField] private bool isSpatialized = false;
+    [SerializeField] private bool isSpatialized = true;
 
     // Start is called before the first frame update
     void Start()
     {
         channelConfig = AkChannelConfig.Standard(2);
-        AkSoundEngine.SetListenerSpatialization(listenerPlayer1, isSpatialized, channelConfig, vVolumes);
+
         vVolumes[0] = 0f;
         vVolumes[1] = -96f;
-        AkSoundEngine.SetListenerSpatialization(listenerPlayer2, isSpatialized, channelConfig, vVolumes);
+        AkSoundEngine.SetListenerSpatialization(listenerPlayer1, isSpatialized, channelConfig, vVolumes);
+
         vVolumes[0] = -96f;
         vVolumes[1] = 0f;
+        AkSoundEngine.SetListenerSpatialization(listenerPlayer2, isSpatialized, channelConfig, vVolumes);
+
+        Debug.Log(AkChannelConfig.Standard(4));
+
+        
 
 
     }
