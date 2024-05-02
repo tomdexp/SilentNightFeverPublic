45ba704fce867b851156cb5f0ca42524542dcaf2
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-05-02 15:08:38 +0200

    Work on Landmark Kitchen

--- Assets/_Project/Scripts/Editor/EditorMenuUtility.cs
+++ Assets/_Project/Scripts/Editor/EditorMenuUtility.cs
@@ -1,4 +1,5 @@
-﻿using _Project.Scripts.Runtime.Player;
+﻿using _Project.Scripts.Runtime.Landmarks.Kitchen;
+using _Project.Scripts.Runtime.Player;
 using UnityEditor;
 using UnityEngine;
 using Logger = _Project.Scripts.Runtime.Utils.Logger;
@@ -19,6 +20,21 @@ namespace _Project.Scripts.Editor
             else
             {
                 Logger.LogError("PlayerData could not be loaded. Please make sure the path is correct.");
+            }
+        }
+
+        [MenuItem("Silent Night Fever/Open LandmarkData_Kitchen")]
+        private static void OpenLandmarkKitchenData()
+        {
+            // Load the PlayerData asset
+            LandmarkData_Kitchen data = AssetDatabase.LoadAssetAtPath<LandmarkData_Kitchen>("Assets/_Project/Settings/Resources/LandmarkDatas/LandmarkData_Kitchen.asset");
+            if (data != null)
+            {
+                PopUpAssetInspector.Create(data);
+            }
+            else
+            {
+                Logger.LogError("LandmarkData_Kitchen could not be loaded. Please make sure the path is correct.");
             }
         }
     }
---
+++ Assets/_Project/Scripts/Runtime/Landmarks/LandmarkData.cs
@@ -0,0 +1,11 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Landmarks
+{
+    public abstract class LandmarkData : ScriptableObject
+    {
+        [Title("Landmark Data")]
+        public LandmarkTag Tags;
+    }
+}
