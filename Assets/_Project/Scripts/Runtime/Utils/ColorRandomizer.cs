93a161aea0678eee67abee8242a1ae97da42571b
Tristan Badana <tristan.badana@etu.unilim.fr>
2024-05-03 08:58:31 +0200

    Default Landmark spawn with random color

---
+++ Assets/_Project/Scripts/Runtime/Utils/ColorRandomizer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using _Project.Scripts.Runtime.Networking;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Utils
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class ColorRandomize : MonoBehaviour
+    {
+        private MeshRenderer[] _meshRenderers;
+        private Color _randomColor;
+
+        private void Awake()
+        {
+            _meshRenderers = GetComponents<MeshRenderer>();
+            foreach (var meshRenderer in _meshRenderers)
+            {
+                meshRenderer.material = new Material(meshRenderer.material);
+            }
+
+            // Générer des valeurs RGB aléatoires
+            float r = UnityEngine.Random.Range(0, 1f);
+            float g = UnityEngine.Random.Range(0, 1f);
+            float b = UnityEngine.Random.Range(0, 1f);
+            // Créer une nouvelle couleur avec les valeurs aléatoires
+            _randomColor = new Color(r, g, b, 1);
+        }
+
+        private void Start()
+        {
+            SetRandomColor();
+        }
+
+        [Button]
+        private void SetRandomColor()
+        {
+
+            foreach (var meshRenderer in _meshRenderers)
+            {
+                meshRenderer.material.SetColor("_Albedo", _randomColor);
+            }
+        }
+    }
+}
