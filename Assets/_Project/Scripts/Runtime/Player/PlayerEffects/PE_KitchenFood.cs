2a76dee086fb18dc5de24aff437c333bf10a86a9
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-05-03 11:08:54 +0200

    Update PE_KitchenFood.cs

--- Assets/_Project/Scripts/Runtime/Player/PlayerEffects/PE_KitchenFood.cs
+++ Assets/_Project/Scripts/Runtime/Player/PlayerEffects/PE_KitchenFood.cs
@@ -8,18 +8,21 @@ namespace _Project.Scripts.Runtime.Player.PlayerEffects
     [CreateAssetMenu(fileName = nameof(PE_KitchenFood), menuName = "Scriptable Objects/" + nameof(PE_KitchenFood))]
     public class PE_KitchenFood : PlayerEffect
     {
-        [Title("PE_KitchenFood Settings")]
-        [SerializeField] private EffectStep[] _effectSteps;
-
-        [Serializable]
-        public class EffectStep
-        {
-            public float SpeedMultiplier;
-            public float SizeMultiplier;
-        }
+        [Title("PE_KitchenFood Settings")] [SerializeField]
+        private float _sizeMultiplier = 1.2f;
+        // [SerializeField] private EffectStep[] _effectSteps;
+        //
+        // [Serializable]
+        // public class EffectStep
+        // {
+        //     public float SpeedMultiplier;
+        //     public float SizeMultiplier;
+        // }
         public override void ApplyEffect(NetworkPlayer player)
         {
-            throw new System.NotImplementedException();
+            var currentPlayerSize = player.transform.localScale.x;
+            var newPlayerSize = currentPlayerSize * _sizeMultiplier;
+            player.TrySetSize(newPlayerSize);
         }
     }
 }
