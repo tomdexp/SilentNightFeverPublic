c5a9a34d02e8679638878bc372363a31c689d146
Seedate <138722515+Seedate@users.noreply.github.com>
2024-05-06 08:50:26 +0200

    adding Listener Manager

--- Assets/_Project/Scripts/Audio/WwPlayerPanning.cs
+++ Assets/_Project/Scripts/Audio/WwPlayerPanning.cs
@@ -1,36 +1,47 @@
+using _Project.Scripts.Runtime.Networking;
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WwPlayerPanning : MonoBehaviour
 {
+    [Required] public NetworkPlayer NetworkPlayer;
 
-    private float[] vVolumes = new float [2];
-    private AkChannelConfig channelConfig;
-
-    public GameObject listenerPlayer1;
-    public GameObject listenerPlayer2;
-
+    private float[] vVolumes = new float[2];
+    private AkChannelConfig channelConfig = new AkChannelConfig();
 
     [SerializeField] private bool isSpatialized = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        channelConfig = AkChannelConfig.Standard(2);
+        channelConfig = AkChannelConfig.Standard(AkSoundEngine.AK_SPEAKER_SETUP_STEREO);
 
-        vVolumes[0] = 0f;
-        vVolumes[1] = -96f;
-        AkSoundEngine.SetListenerSpatialization(listenerPlayer1, isSpatialized, channelConfig, vVolumes);
+        vVolumes[0] = 0;
+        vVolumes[1] = -14;
+        AkSoundEngine.SetListenerSpatialization(this.gameObject, isSpatialized, channelConfig, vVolumes);
 
-        Debug.Log(" le Volumes est de " + vVolumes[1]);
-        Debug.Log("AkChannelConfig est : " + channelConfig);
 
-        vVolumes[0] = -96f;
-        vVolumes[1] = 0f;
-        AkSoundEngine.SetListenerSpatialization(listenerPlayer2, isSpatialized, channelConfig, vVolumes);
 
-        
+        switch (NetworkPlayer.GetPlayerIndexType())
+        {
+            case _Project.Scripts.Runtime.Player.PlayerIndexType.A:
+                PlayerABoffset();
+                break;
+            case _Project.Scripts.Runtime.Player.PlayerIndexType.B:
+                PlayerABoffset();
+                break;
+            case _Project.Scripts.Runtime.Player.PlayerIndexType.C:
+                PlayerCDoffset();
+                break;
+            case _Project.Scripts.Runtime.Player.PlayerIndexType.D:
+                PlayerCDoffset();
+                break;
+            case _Project.Scripts.Runtime.Player.PlayerIndexType.Z:
+
+                break;
+        }
 
 
     }
@@ -40,4 +51,17 @@ public class WwPlayerPanning : MonoBehaviour
     {
         
     }
+
+    private void PlayerABoffset()
+    {
+        vVolumes[0] = 0;
+        vVolumes[1] = -96;
+    }
+
+    private void PlayerCDoffset()
+    {
+        vVolumes[0] = -96;
+        vVolumes[1] = 0;
+    }
+
 }
