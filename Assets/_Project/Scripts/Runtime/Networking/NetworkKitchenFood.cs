87091bc9b2314d0ed61f4a5cdfd96b0455a5277f
Tom D EXPORT <71851742+tomdexp@users.noreply.github.com>
2024-05-08 16:59:51 +0200

    Fix Kitchen Food not despawning on consumed

--- Assets/_Project/Scripts/Runtime/Networking/NetworkKitchenFood.cs
+++ Assets/_Project/Scripts/Runtime/Networking/NetworkKitchenFood.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using _Project.Scripts.Runtime.Player.PlayerEffects;
 using _Project.Scripts.Runtime.Player.PlayerTongue;
+using FishNet;
+using FishNet.Object;
 using UnityEngine;
 using Logger = _Project.Scripts.Runtime.Utils.Logger;
 
@@ -18,6 +20,16 @@ namespace _Project.Scripts.Runtime.Networking
         {
             yield return new WaitForSeconds(2f);
             PlayerManager.Instance.TryGiveEffectToPlayer<PE_KitchenFood>(tongue.GetNetworkPlayer().GetPlayerIndexType());
+            DespawnServerRpc();
+        }
+        
+        [ServerRpc(RequireOwnership = false)]
+        private void DespawnServerRpc()
+        {
+            if (IsServerStarted)
+            {
+                Despawn();
+            }
         }
     }
 }
