1fe8d0fd7af7972bf2d007f873eefda81156dd6d
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-05-22 19:22:28 +0200

    Add SyncEvent custom sync type

---
+++ Assets/_Project/Scripts/Runtime/Utils/SyncEventTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using _Project.Scripts.Runtime.Networking;
+using FishNet.Object;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Utils
+{
+    public class SyncEventTest : NetworkBehaviour
+    {
+        public readonly SyncEvent OnTest = new SyncEvent();
+
+        private void Start()
+        {
+            OnTest.OnEvent += OnTestEventFired;
+        }
+
+        private void OnTestEventFired(bool asServer)
+        {
+            Debug.Log($"OnTestEventFired: {asServer} with client id: {NetworkManager.ClientManager.Connection.ClientId}");
+        }
+
+        [Button(ButtonSizes.Large)]
+        private void InvokeTestEvent()
+        {
+            InvokeServerRpc();
+        }
+        
+        [ServerRpc(RequireOwnership = false)]
+        private void InvokeServerRpc()
+        {
+            OnTest.Invoke();
+        }
+    }
+}
