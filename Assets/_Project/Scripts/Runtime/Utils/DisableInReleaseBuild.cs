182da3ed11d8d856b5ed1548bda787db2d3aaa6a
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-05-23 10:21:03 +0200

    Add Platforms Package with Config Builds
    Remove mesh of player spawn points

---
+++ Assets/_Project/Scripts/Runtime/Utils/DisableInReleaseBuild.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Utils
+{
+    public class DisableInReleaseBuild : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (Debug.isDebugBuild) return;
+            Logger.LogTrace($"Disabling GameObject ({gameObject.name} in Release Build", Logger.LogType.Local, this);
+            gameObject.SetActive(false);
+        }
+    }
+}
