abf27992aa78eefeb4c502607702811cb1536f05
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-05-23 13:48:05 +0200

    Fix ColorRandomizer.cs null ref on remote client

--- Assets/_Project/Scripts/Runtime/Utils/ColorRandomizer.cs
+++ Assets/_Project/Scripts/Runtime/Utils/ColorRandomizer.cs
@@ -12,48 +12,54 @@ namespace _Project.Scripts.Runtime.Utils
     public class ColorRandomize : NetworkBehaviour
     {
         private MeshRenderer[] _meshRenderers;
-        [AllowMutableSyncType]  private SyncVar<Color> _randomColor;
+        private readonly SyncVar<Color> _randomColor = new SyncVar<Color>();
 
-        private void OnEnable()
-        {
-            _randomColor.OnChange += SetRandomColor;
-        }
-        private void OnDisable()
-        {
-            _randomColor.OnChange -= SetRandomColor;
-        }
-
-        private void SetRandomColor(Color prev, Color next, bool asServer)
-        {
-            SetRandomColor();
-        }
-
-        private void SetRandomColor()
-        {
-            foreach (var meshRenderer in _meshRenderers)
-            {
-                meshRenderer.material.color = _randomColor.Value;
-            }
-        }
-
-        private void Start()
+        private void Awake()
         {
             _meshRenderers = GetComponentsInChildren<MeshRenderer>();
             foreach (var meshRenderer in _meshRenderers)
             {
                 meshRenderer.material = new Material(meshRenderer.material);
             }
+        }
+        
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            float r = UnityEngine.Random.Range(0, 1f);
+            float g = UnityEngine.Random.Range(0, 1f);
+            float b = UnityEngine.Random.Range(0, 1f);
+            _randomColor.Value = new Color(r, g, b, 1);
+        }
 
-            SetRandomColor();
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            SetRandomColor(_randomColor.Value);
+        }
 
-            if (IsServerStarted)
+        private void Start()
+        {
+            _randomColor.OnChange += SetRandomColor;
+        }
+
+
+        private void OnDestroy()
+        {
+            _randomColor.OnChange -= SetRandomColor;
+        }
+
+        private void SetRandomColor(Color prev, Color next, bool asServer)
+        {
+            SetRandomColor(next);
+        }
+
+        private void SetRandomColor(Color newColor)
+        {
+            foreach (var meshRenderer in _meshRenderers)
             {
-                float r = UnityEngine.Random.Range(0, 1f);
-                float g = UnityEngine.Random.Range(0, 1f);
-                float b = UnityEngine.Random.Range(0, 1f);
-                _randomColor.Value = new Color(r, g, b, 1);
+                meshRenderer.material.color = newColor;
             }
-
         }
     }
 }
