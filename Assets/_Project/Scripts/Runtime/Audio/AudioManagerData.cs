a57cf0a4e0aa479fb377875b210572de8b0e31be
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-05-24 14:50:52 +0200

    Improve AudioManagerData UI

--- Assets/_Project/Scripts/Runtime/Audio/AudioManagerData.cs
+++ Assets/_Project/Scripts/Runtime/Audio/AudioManagerData.cs
@@ -8,86 +8,120 @@ namespace _Project.Scripts.Runtime.Audio
     [CreateAssetMenu(fileName = nameof(AudioManagerData), menuName = "Scriptable Objects/" + nameof(AudioManagerData))]
     public class AudioManagerData : ScriptableObject
     {
-        [Title("Global Settings")]
+        [TabGroup("Settings","Global", SdfIconType.GearFill, TextColor = "green")]
         public Logger.LogLevel EventNotFoundLogLevel = Logger.LogLevel.Warning;
+        [TabGroup("Settings","Global", SdfIconType.GearFill, TextColor = "green")]
         public Logger.LogLevel RTPCNotFoundLogLevel = Logger.LogLevel.Warning;
+        [TabGroup("Settings","Global", SdfIconType.GearFill, TextColor = "green")]
         [Tooltip("If False, the AudioManager will not log any RTPC, errors or not")]
         public bool RPTCLog = false;
         
-        [Title("Banks Settings")]
+        [TabGroup("Settings","Banks", SdfIconType.SafeFill, TextColor = "orange")]
         public BankLoadConfig[] BanksToLoadOnApplicationStart;
         
-        [Title("Players Panning Settings")]
+        [TabGroup("Settings","Volumes Offset", SdfIconType.SpeakerFill, TextColor = "yellow")]
+        [ListDrawerSettings(HideAddButton = true, HideRemoveButton = true)]
         public float[] LeftSpeakerVolumeOffset = new float[2] {0, -96}; 
+        [TabGroup("Settings","Volumes Offset", SdfIconType.SpeakerFill, TextColor = "yellow")]
+        [ListDrawerSettings(HideAddButton = true, HideRemoveButton = true)]
         public float[] RightSpeakerVolumeOffset = new float[2] {-96, 0};
         
-        [Title("Wwise RTPC", "GP_MUSC_SWITCH")]
+        [TabGroup("Settings","RTPC", SdfIconType.Sliders, TextColor = "lightcyan")]
+        [TabGroup("Settings/RTPC/Subgroup", "GP_MUSC_SWITCH", SdfIconType.MusicNoteBeamed, TextColor = "darkcyan")]
         [Tooltip("This RTPC value will change on any new round, so that the music switch to a new track")]
         public AK.Wwise.RTPC RTPC_GP_MUSC_SWITCH;
+        [TabGroup("Settings/RTPC/Subgroup", "GP_MUSC_SWITCH", SdfIconType.MusicNoteBeamed, TextColor = "darkcyan")]
         [Tooltip("Inclusive Min Value (which means a number will be generated between Min and Max, including Min and Max)")]
         public int RTPC_GP_MUSC_SWITCH_MinValue = 1;
+        [TabGroup("Settings/RTPC/Subgroup", "GP_MUSC_SWITCH", SdfIconType.MusicNoteBeamed, TextColor = "darkcyan")]
         [Tooltip("Inclusive Max Value (which means a number will be generated between Min and Max, including Min and Max)")]
         public int RTPC_GP_MUSC_SWITCH_MaxValue = 4;
         
-        [Title("Wwise RTPC", "GP_LM_SatelliteSpeed")]
+        [TabGroup("Settings","RTPC", SdfIconType.Sliders, TextColor = "lightcyan")]
+        [TabGroup("Settings/RTPC/Subgroup", "GP_LM_SatelliteSpeed", SdfIconType.Speedometer, TextColor = "darkgreen")]
         public AK.Wwise.RTPC RTPC_GP_LM_SatelliteSpeed;
+        [TabGroup("Settings/RTPC/Subgroup", "GP_LM_SatelliteSpeed", SdfIconType.Speedometer, TextColor = "darkgreen")]
         public float RTPC_GP_LM_SatelliteSpeed_MinValue = 0f;
+        [TabGroup("Settings/RTPC/Subgroup", "GP_LM_SatelliteSpeed", SdfIconType.Speedometer, TextColor = "darkgreen")]
         public float RTPC_GP_LM_SatelliteSpeed_MaxValue = 10f;
         
-        [Title("AkEvent References", "Global Events")]
+        [TabGroup("References", "Events", SdfIconType.MusicNote, TextColor = "lightmagenta")]
+        [TabGroup("References/Events/Subgroup", "Application", SdfIconType.Bullseye, TextColor = "magenta")]
         [Tooltip("Called at the start of the application, will only be called ONCE, when the application start, not when the game start, or a round start")]
         public AK.Wwise.Event EventApplicationStart;
+        [TabGroup("References/Events/Subgroup", "Application", SdfIconType.Bullseye, TextColor = "magenta")]
         [Tooltip("Called when the Main Menu scene starts, it can be called MULTIPLE times, when the scene is loaded again, or when the game is restarted")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventMainMenuStart;
+        [TabGroup("References/Events/Subgroup", "Application", SdfIconType.Bullseye, TextColor = "magenta")]
         [Tooltip("Called when the Character Selection menu pops up")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventCharacterSelectionStart;
+        [TabGroup("References/Events/Subgroup", "Application", SdfIconType.Bullseye, TextColor = "magenta")]
         [Tooltip("Called when the whole game session start, so that the map is generated and its the first round")]
         public AK.Wwise.Event EventGameStart;
+        [TabGroup("References/Events/Subgroup", "Application", SdfIconType.Bullseye, TextColor = "magenta")]
         [Tooltip("Called when the game end, so when the last round end, and the game is over")]
         public AK.Wwise.Event EventGameEnd;
+        [TabGroup("References/Events/Subgroup", "Application", SdfIconType.Bullseye, TextColor = "magenta")]
         [Tooltip("Called when a round start, there is a delay between when a round end, and a round start to show UI")]
         public AK.Wwise.Event EventRoundStart;
+        [TabGroup("References/Events/Subgroup", "Application", SdfIconType.Bullseye, TextColor = "magenta")]
         [Tooltip("Called when a round end, so when a Team wins, will be called for the first and last round too")]
         public AK.Wwise.Event EventRoundEnd;
 
-        [Title("AkEvent References", "Player Events")]
+        [TabGroup("References", "Events", SdfIconType.MusicNote, TextColor = "lightmagenta")]
+        [TabGroup("References/Events/Subgroup", "Player", SdfIconType.PeopleFill, TextColor = "lightgreen")]
         public AK.Wwise.Event EventPlayerTongueThrow;
+        [TabGroup("References/Events/Subgroup", "Player", SdfIconType.PeopleFill, TextColor = "lightgreen")]
         public AK.Wwise.Event EventPlayerTongueRetract;
+        [TabGroup("References/Events/Subgroup", "Player", SdfIconType.PeopleFill, TextColor = "lightgreen")]
         public AK.Wwise.Event EventPlayerTongueInteractOrBind;
         
-        [Title("AkEvent References", "UI Events")]
+        [TabGroup("References", "Events", SdfIconType.MusicNote, TextColor = "lightmagenta")]
+        [TabGroup("References/Events/Subgroup", "UI", SdfIconType.CursorFill, TextColor = "lightblue")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventUIButtonHover;
+        [TabGroup("References/Events/Subgroup", "UI", SdfIconType.CursorFill, TextColor = "lightblue")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventUIButtonClickEnter;
+        [TabGroup("References/Events/Subgroup", "UI", SdfIconType.CursorFill, TextColor = "lightblue")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventUIButtonClickBack;
+        [TabGroup("References/Events/Subgroup", "UI", SdfIconType.CursorFill, TextColor = "lightblue")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventUIOnlineLobbyCreated;
+        [TabGroup("References/Events/Subgroup", "UI", SdfIconType.CursorFill, TextColor = "lightblue")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventUIOnlineLobbyPlayerJoin;
+        [TabGroup("References/Events/Subgroup", "UI", SdfIconType.CursorFill, TextColor = "lightblue")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventUIPlayerJoinWithController;
+        [TabGroup("References/Events/Subgroup", "UI", SdfIconType.CursorFill, TextColor = "lightblue")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventUIPlayerLeftWithController;
         
-        [Title("AkEvent References", "Landmark Events")]
+        [TabGroup("References", "Events", SdfIconType.MusicNote, TextColor = "lightmagenta")]
+        [TabGroup("References/Events/Subgroup", "Landmarks", SdfIconType.PinMapFill, TextColor = "lightorange")]
         [InfoBox("Not Implemented Yet", InfoMessageType.Error)]
         public AK.Wwise.Event EventLandmarkKitchenFoodEaten;
+        [TabGroup("References/Events/Subgroup", "Landmarks", SdfIconType.PinMapFill, TextColor = "lightorange")]
         public AK.Wwise.Event EventLandmarkZoomStartTurning;
+        [TabGroup("References/Events/Subgroup", "Landmarks", SdfIconType.PinMapFill, TextColor = "lightorange")]
         public AK.Wwise.Event EventLandmarkZoomStopTurning;
         
         
         [Serializable]
         public class BankLoadConfig
         {
+            [TableColumnWidth(60)]
             public AK.Wwise.Bank Bank;
+            [TableColumnWidth(20)]
             [Tooltip("If true, decode this SoundBank upon load")]
-            public bool DecodeBankOnLoad = true;
+            public bool DecodeBankOnLoad = false;
+            [TableColumnWidth(20)]
             [Tooltip("If true, save the decoded SoundBank to disk for faster loads in the future")]
-            public bool SaveDecodedBank = true;
+            public bool SaveDecodedBank = false;
 
             public void Load()
             {
