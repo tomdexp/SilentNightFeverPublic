0d135100bc85faa3330c513cd99f3df971bac168
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-05-28 16:34:19 +0200

    Improve tongue ally targeting with radial aim assist

--- Assets/_Project/Scripts/Runtime/Utils/FilterTongueColliderProcessor.cs
+++ Assets/_Project/Scripts/Runtime/Utils/FilterTongueColliderProcessor.cs
@@ -1,11 +1,11 @@
 ﻿using _Project.Scripts.Runtime.Player.PlayerTongue;
 using Micosmo.SensorToolkit;
 
+
 namespace _Project.Scripts.Runtime.Utils
 {
     public class FilterTongueColliderProcessor : SignalProcessor
     {
-        // If the signal object has a TongueCollider component, return true
         public override bool Process(ref Signal signal, Sensor sensor)
         {
             return signal.Object.TryGetComponent(out TongueCollider tongueCollider);
