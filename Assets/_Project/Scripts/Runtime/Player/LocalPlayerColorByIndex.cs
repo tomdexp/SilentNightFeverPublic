3a95647c4c51c892f29b1ffec9fef856e1f02cb6
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-06-14 11:09:17 +0200

    Create new LocalPlayerColorByIndex script

---
+++ Assets/_Project/Scripts/Runtime/Player/LocalPlayerColorByIndex.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Runtime.Player
+{
+    public class LocalPlayerColorByIndex : MonoBehaviour
+    {
+        [field:SerializeField] public PlayerIndexType PlayerIndexType { get; private set; }
+        [SerializeField] private PlayerData _playerData;
+        [SerializeField] private MeshRenderer[] _meshRenderers;
+        [SerializeField] private SkinnedMeshRenderer[] _skinnedMeshRenderers;
+
+        private static readonly int BodyColorParam = Shader.PropertyToID("_Body_Color");
+
+        private void Update()
+        {
+            SetColorByPlayerIndex();
+        }
+
+        private void SetColorByPlayerIndex()
+        {
+            switch (PlayerIndexType)
+            {
+                case PlayerIndexType.A:
+                    SetColor(_playerData.PlayerAColor);
+                    break;
+                case PlayerIndexType.B:
+                    SetColor(_playerData.PlayerBColor);
+                    break;
+                case PlayerIndexType.C:
+                    SetColor(_playerData.PlayerCColor);
+                    break;
+                case PlayerIndexType.D:
+                    SetColor(_playerData.PlayerDColor);
+                    break;
+            }
+        }
+
+        private void SetColor(Color color)
+        {
+            foreach (var meshRenderer in _meshRenderers) meshRenderer.material.color = color;
+            foreach (var skinnedMeshRenderer in _skinnedMeshRenderers)
+            {
+                // set the "_Body_Color" property of the shader
+                skinnedMeshRenderer.material.SetColor(BodyColorParam, color);
+            }
+        }
+    }
+}
