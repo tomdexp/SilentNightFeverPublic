a8bc25b207bf32593f629d5acf790a0c29c359d2
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-06-19 15:48:16 +0200

    Replicated Transitions and added random text

---
+++ Assets/_Project/Scripts/Runtime/Networking/TransitionBroadcast.cs
@@ -0,0 +1,10 @@
+﻿using FishNet.Broadcast;
+
+namespace _Project.Scripts.Runtime.Networking
+{
+    public struct TransitionBroadcast : IBroadcast
+    {
+        public string Id;
+        public bool Open;
+    }
+}
---
+++ Assets/_Project/Scripts/Runtime/UI/UI_BindRandomText.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using Random = UnityEngine.Random;
+
+namespace _Project.Scripts.Runtime.UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class UI_BindRandomText : MonoBehaviour
+    {
+        [SerializeField] private LocalizedString[] _availableStrings;
+        
+        private TMP_Text _text;
+        private int _currentIndex;
+
+        private void Start()
+        {
+            _text = GetComponent<TMP_Text>();
+            PickRandomText();
+            LocalizationSettings.Instance.OnSelectedLocaleChanged += OnLocaleChanged;
+        }
+
+        private void OnDestroy()
+        {
+            LocalizationSettings.Instance.OnSelectedLocaleChanged -= OnLocaleChanged;
+        }
+        
+        private void OnLocaleChanged(Locale _)
+        {
+            UpdateText();
+        }
+
+        public void PickRandomText()
+        {
+            if (_availableStrings.Length == 0) return;
+            _currentIndex = Random.Range(0, _availableStrings.Length);
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            _text.text = _availableStrings[_currentIndex].GetLocalizedString();
+        }
+    }
+}
