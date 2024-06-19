4d0cc488a77378afe6f4c41766c3bbdff035d92b
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-06-19 10:30:11 +0200

    Fix Character Customization not showing

--- Assets/_Project/Scripts/Runtime/CharacterCustomization/CharacterCustomizer.cs
+++ Assets/_Project/Scripts/Runtime/CharacterCustomization/CharacterCustomizer.cs
@@ -1,130 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using _Project.Scripts.Runtime.Networking;
 using _Project.Scripts.Runtime.Player;
-using FishNet;
-using FishNet.Transporting;
 using UnityEngine;
 using Logger = _Project.Scripts.Runtime.Utils.Logger;
 
-public class CharacterCustomizer : MonoBehaviour
+namespace _Project.Scripts.Runtime.CharacterCustomization
 {
-    [SerializeField] GameObject _playerAMannequin;
-    [SerializeField] GameObject _playerAMannequinArrows;
-    [Space]
-    [SerializeField] GameObject _playerBMannequin;
-    [SerializeField] GameObject _playerBMannequinArrows;
-    [Space]
-    [SerializeField] GameObject _playerCMannequin;
-    [SerializeField] GameObject _playerCMannequinArrows;
-    [Space]
-    [SerializeField] GameObject _playerDMannequin;
-    [SerializeField] GameObject _playerDMannequinArrows;
-
-    private void Awake()
+    public class CharacterCustomizer : MonoBehaviour
     {
-        InstanceFinder.ClientManager.OnClientConnectionState += OnClientConnectionState;
-        StopCustomization();
-    }
+        [SerializeField] GameObject _playerAMannequin;
+        [SerializeField] GameObject _playerAMannequinArrows;
+        [Space]
+        [SerializeField] GameObject _playerBMannequin;
+        [SerializeField] GameObject _playerBMannequinArrows;
+        [Space]
+        [SerializeField] GameObject _playerCMannequin;
+        [SerializeField] GameObject _playerCMannequinArrows;
+        [Space]
+        [SerializeField] GameObject _playerDMannequin;
+        [SerializeField] GameObject _playerDMannequinArrows;
 
-    private void OnClientConnectionState(ClientConnectionStateArgs args)
-    {
-        if (PlayerManager.HasInstance)
+        private void Awake()
         {
-            PlayerManager.Instance.OnPlayerHatInfosChanged -= OnPlayerHatInfosChanged;
-            PlayerManager.Instance.OnCharacterCustomizationStarted -= StartCustomization;
-            Logger.LogTrace("Unsubscribed from PlayerManager events", Logger.LogType.Client, this);
+            StopCustomization();
         }
 
-        if (args.ConnectionState == LocalConnectionState.Started)
+        public void StartCustomization()
         {
-            StartCoroutine(TrySubscribingToEvents());
+            Logger.LogTrace("Starting customization by activating mannequin", Logger.LogType.Client, this);
+            _playerAMannequin.gameObject.SetActive(true);
+            _playerBMannequin.gameObject.SetActive(true);
+            _playerCMannequin.gameObject.SetActive(true);
+            _playerDMannequin.gameObject.SetActive(true);
         }
-    }
 
-    private void OnDestroy()
-    {
-        if (PlayerManager.HasInstance) PlayerManager.Instance.OnPlayerHatInfosChanged -= OnPlayerHatInfosChanged;
-        if (PlayerManager.HasInstance) PlayerManager.Instance.OnCharacterCustomizationStarted -= StartCustomization;
-    }
-
-    private IEnumerator TrySubscribingToEvents()
-    {
-        while (!PlayerManager.HasInstance)
+        public void StopCustomization()
         {
-            yield return null;
+            Logger.LogTrace("Stopping customization by disabling mannequin", Logger.LogType.Client, this);
+            _playerAMannequin.gameObject.SetActive(false);
+            _playerBMannequin.gameObject.SetActive(false);
+            _playerCMannequin.gameObject.SetActive(false);
+            _playerDMannequin.gameObject.SetActive(false);
         }
-        PlayerManager.Instance.OnCharacterCustomizationStarted += StartCustomization;
-        Logger.LogTrace("Subscribed to PlayerManager events", Logger.LogType.Client, this);
-        PlayerManager.Instance.OnPlayerHatInfosChanged += OnPlayerHatInfosChanged;
-    }
 
-    private void StartCustomization()
-    {
-        Logger.LogTrace("Starting customization by set active mannequin", Logger.LogType.Client, this);
-        _playerAMannequin.gameObject.SetActive(true);
-        _playerBMannequin.gameObject.SetActive(true);
-        _playerCMannequin.gameObject.SetActive(true);
-        _playerDMannequin.gameObject.SetActive(true);
-    }
-
-    private void StopCustomization()
-    {
-        _playerAMannequin.gameObject.SetActive(false);
-        _playerBMannequin.gameObject.SetActive(false);
-        _playerCMannequin.gameObject.SetActive(false);
-        _playerDMannequin.gameObject.SetActive(false);
-    }
-
-    private void OnPlayerHatInfosChanged(List<PlayerHatInfo> hatInfos)
-    {
-        foreach (var hatInfo in hatInfos)
+        private void Update()
         {
-            switch (hatInfo.PlayerIndexType)
+            if (Time.frameCount % 10 == 0)
             {
-                case PlayerIndexType.A:
-                    if (hatInfo.HasConfirmed)
-                    {
-                        _playerAMannequinArrows.gameObject.SetActive(false);
-                    }
-                    else
-                    {
-                        _playerAMannequinArrows.gameObject.SetActive(true);
-                    }
-                    break;
+                UpdateMannequins();
+            }
+        }
 
-                case PlayerIndexType.B:
-                    if (hatInfo.HasConfirmed)
-                    {
-                        _playerBMannequinArrows.gameObject.SetActive(false);
-                    }
-                    else
-                    {
-                        _playerBMannequinArrows.gameObject.SetActive(true);
-                    }
-                    break;
+        private void UpdateMannequins()
+        {
+            if (!PlayerManager.HasInstance)
+            {
+                StopCustomization();
+                return;
+            }
+        
+            foreach (var hatInfo in PlayerManager.Instance.GetPlayerHatInfos())
+            {
+                switch (hatInfo.PlayerIndexType)
+                {
+                    case PlayerIndexType.A:
+                        _playerAMannequinArrows.gameObject.SetActive(!hatInfo.HasConfirmed);
+                        break;
 
-                case PlayerIndexType.C:
-                    if (hatInfo.HasConfirmed)
-                    {
-                        _playerCMannequinArrows.gameObject.SetActive(false);
-                    }
-                    else
-                    {
-                        _playerCMannequinArrows.gameObject.SetActive(true);
-                    }
-                    break;
+                    case PlayerIndexType.B:
+                        _playerBMannequinArrows.gameObject.SetActive(!hatInfo.HasConfirmed);
+                        break;
 
-                case PlayerIndexType.D:
-                    if (hatInfo.HasConfirmed)
-                    {
-                        _playerDMannequinArrows.gameObject.SetActive(false);
-                    }
-                    else
-                    {
-                        _playerDMannequinArrows.gameObject.SetActive(true);
-                    }
-                    break;
+                    case PlayerIndexType.C:
+                        _playerCMannequinArrows.gameObject.SetActive(!hatInfo.HasConfirmed);
+                        break;
+
+                    case PlayerIndexType.D:
+                        _playerDMannequinArrows.gameObject.SetActive(!hatInfo.HasConfirmed);
+                        break;
+                    case PlayerIndexType.Z:
+                        break;
+                }
             }
         }
     }
--- Assets/_Project/Scripts/Runtime/UI/NetworkedMenu/CustomizationMenu.cs
+++ Assets/_Project/Scripts/Runtime/UI/NetworkedMenu/CustomizationMenu.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using _Project.Scripts.Runtime.Audio;
+using _Project.Scripts.Runtime.CharacterCustomization;
 using _Project.Scripts.Runtime.Networking;
 using _Project.Scripts.Runtime.Utils;
 using FishNet;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Logger = _Project.Scripts.Runtime.Utils.Logger;
 
 namespace _Project.Scripts.Runtime.UI.NetworkedMenu
 {
@@ -27,6 +28,15 @@ namespace _Project.Scripts.Runtime.UI.NetworkedMenu
             UIManager.Instance.SwitchToCustomizationCamera();
             if (AudioManager.HasInstance) AudioManager.Instance.PlayAudioLocal(AudioManager.Instance.AudioManagerData.EventCharacterCustomizationStart, AudioManager.Instance.gameObject);
             _canvasGroup.Open();
+            var characterCustomizer = FindAnyObjectByType<CharacterCustomizer>();
+            if (characterCustomizer)
+            {
+                characterCustomizer.StartCustomization();
+            }
+            else
+            {
+                Logger.LogError("No CharacterCustomizer found in the scene", Logger.LogType.Client, this);
+            }
             if (InstanceFinder.IsServerStarted)
             {
                 if(PlayerManager.HasInstance) PlayerManager.Instance.TryStartCharacterCustomization();
@@ -50,6 +60,15 @@ namespace _Project.Scripts.Runtime.UI.NetworkedMenu
         {
             base.Close();
             _canvasGroup.Close();
+            var characterCustomizer = FindAnyObjectByType<CharacterCustomizer>();
+            if (characterCustomizer)
+            {
+                characterCustomizer.StopCustomization();
+            }
+            else
+            {
+                Logger.LogError("No CharacterCustomizer found in the scene", Logger.LogType.Client, this);
+            }
             if (InstanceFinder.IsServerStarted)
             {
                 if(PlayerManager.HasInstance) PlayerManager.Instance.TryStopCharacterCustomization();
