e5673a51e7201301c0c3f9febb4c6c1f2420b889
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-06-19 10:56:30 +0200

    Add FakeBrand text to UI

--- Assets/_Project/Scripts/Runtime/UI/NetworkedMenu/ParametersGraphicsMenu.cs
+++ Assets/_Project/Scripts/Runtime/UI/NetworkedMenu/ParametersGraphicsMenu.cs
@@ -12,7 +12,6 @@ namespace _Project.Scripts.Runtime.UI.NetworkedMenu
     {
         public override string MenuName { get; } = "ParametersGraphicsMenu";
         [SerializeField, Required] private UI_Toggle _highContrastToggle;
-        [SerializeField, Required] private UI_Toggle _epilepsyModeToggle;
         [SerializeField, Required] private UI_Toggle _fullScreenToggle;
         [SerializeField, Required] private UI_Toggle _vsyncToggle;
         [SerializeField, Required] private UI_SelectorBase _resolutionSlider;
@@ -20,7 +19,6 @@ namespace _Project.Scripts.Runtime.UI.NetworkedMenu
         private CanvasGroup _canvasGroup;
         
         private Toggle _highContrastToggleComponent;
-        private Toggle _epilepsyModeToggleComponent;
         private Toggle _fullScreenToggleComponent;
         private Toggle _vsyncToggleComponent;
         private Button _resolutionSliderPreviousButton;
@@ -32,15 +30,13 @@ namespace _Project.Scripts.Runtime.UI.NetworkedMenu
             _canvasGroup.CloseInstant();
 
             _highContrastToggleComponent = _highContrastToggle.GetComponentInChildren<Toggle>();
-            _epilepsyModeToggleComponent = _epilepsyModeToggle.GetComponentInChildren<Toggle>();
             _fullScreenToggleComponent = _fullScreenToggle.GetComponentInChildren<Toggle>();
             _vsyncToggleComponent = _vsyncToggle.GetComponentInChildren<Toggle>();
             _resolutionSliderPreviousButton = _resolutionSlider.PreviousButton;
             _resolutionSliderNextButton = _resolutionSlider.NextButton;
             
             BindNavigableVertical(_backButton, _highContrastToggleComponent);
-            BindNavigableVertical(_highContrastToggleComponent, _epilepsyModeToggleComponent);
-            BindNavigableVertical(_epilepsyModeToggleComponent, _fullScreenToggleComponent);
+            BindNavigableVertical(_highContrastToggleComponent, _fullScreenToggleComponent);
             BindNavigableVertical(_fullScreenToggleComponent, _vsyncToggleComponent);
             BindNavigableVertical(_vsyncToggleComponent, _resolutionSliderPreviousButton);
             
